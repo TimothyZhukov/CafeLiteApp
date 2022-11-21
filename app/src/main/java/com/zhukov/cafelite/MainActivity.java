d8c1e4b95bac7136022442a0dd0d1d0fe94b66d8
TimothyZhukov <timazhukov@gmail.com>
2022-11-21 14:31:00 +0200

    design of the third screen + logic of the first screen

--- app/src/main/java/com/zhukov/cafelite/MainActivity.java
+++ app/src/main/java/com/zhukov/cafelite/MainActivity.java
@@ -4,14 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextPassword;
+    private Button buttonSingIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, MakeOrderActivity.class);
+        initViews();
+        buttonSingIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextName.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                if (name.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(MainActivity.this,
+                            R.string.error_fields_empty,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                } else {
+                    launchNextScreen(name);
+                }
+            }
+        });
+    }
+
+    private void launchNextScreen(String name) {
+        Intent intent = MakeOrderActivity.newIntent(this, name);
         startActivity(intent);
+    }
+
+    private void initViews() {
+        editTextName = findViewById(R.id.editTextName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSingIn = findViewById(R.id.buttonSingIn);
     }
 }
--- app/src/main/java/com/zhukov/cafelite/MakeOrderActivity.java
+++ app/src/main/java/com/zhukov/cafelite/MakeOrderActivity.java
@@ -2,7 +2,10 @@ package com.zhukov.cafelite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MakeOrderActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MakeOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order);
+        TextView textViewGreetings = findViewById(R.id.textViewGreetings);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        textViewGreetings.setText(String.format(textViewGreetings.getText().toString(), name));
+    }
+
+    public static Intent newIntent(Context context, String name) {
+        Intent intent = new Intent(context, MakeOrderActivity.class);
+        intent.putExtra("name", name);
+        return intent;
     }
 }
